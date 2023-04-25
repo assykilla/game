68cb6c25f35aa8d8e02d7e7870c44ae55fcc35cc
George <george.torres.641@gmail.com>
2023-04-25 11:05:14 -0700

    modified triangles

--- gtorres.cpp
+++ gtorres.cpp
@@ -10,6 +10,7 @@
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include "fonts.h"
+#include "xreyes.h"
 using namespace std;
 
 int points = 0;
@@ -27,93 +28,37 @@ void renderStats(Rect r, int x, int y, int pts) {
     ggprint8b(&r, 16, 0x0000c8f5, "Points: %i", pts);
 }
 
-float Gt1[] = 
-{
-    5.0f, 50.0f,
-    150.0f, 50.0f,
-    5.0f, 100.0f
-};
 
-float Gt2[] =
-{
-    545.0f, 100.0f,
-    545.0f, 50.0f,
-    400.0f, 50.0f
-};
+Triangle Gt1 = Triangle (5.0f,150.0f, 5.0f, 
+                        50.0f ,50.0f ,100.0f);
+Triangle Gt2 = Triangle (545.0f,545.0f, 400.0f, 
+                        100.0f ,50.0f ,50.0f);
+Triangle Gt3 = Triangle (600.0f,600.0f, 500.0f, 
+                        700.0f ,800.0f ,800.0f);
+Triangle Gt4 = Triangle (105.0f,5.0f, 5.0f, 
+                        795.0f ,695.0f ,795.0f);
+Triangle Gt5 = Triangle (225.0f,225.0f, 200.0f, 
+                        795.0f ,695.0f ,795.0f);
+Triangle Gt6 = Triangle (225.0f,225.0f, 250.0f,
+                        795.0f ,695.0f ,795.0f);
+Triangle Gt7 = Triangle (80.0f,5.0f, 5.0f, 
+                        550.0f ,550.0f ,600.0f);
+Triangle Gt8 = Triangle (80.0f,5.0f, 5.0f, 
+                        550.0f ,300.0f ,550.0f);
+Triangle Gt9 = Triangle (545.0f,545.0f, 420.0f, 
+                        650.0f ,500.0f ,500.0f);
+Triangle Gt10 = Triangle (545.0f,545.0f, 420.0f, 
+                        250.0f ,500.0f ,500.0f);
+Triangle Gt11 = Triangle (150.0f,60.0f, 60.0f, 
+                        100.0f ,135.0f ,275.0f);
+Triangle Gt12 = Triangle (490.0f,490.0f, 400.0f, 
+                        275.0f ,135.0f ,100.0f);
 
-float Gt3[] =
-{
-    600.0f, 700.0f,
-    600.0f, 800.0f,
-    500.0f, 800.0f
-};
 
-float Gt4[] =
+void addScore(int amount)
 {
-    105.0f, 795.0f,
-    5.0f, 695.0f,
-    5.0f, 795.0f
-};
-
-float Gt5[] =
-{
-    225.0f, 795.0f,
-    225.0f, 695.0f,
-    200.0f, 795.0f
-};
-
-float Gt6[] =
-{
-    225.0f, 795.0f,
-    225.0f, 695.0f,
-    250.0f, 795.0f
-};
-
-float Gt7[] =
-{
-    80.0f, 550.0f,
-    5.0f, 550.0f,
-    5.0f, 600.0f
-};
-
-float Gt8[] =
-{
-    80.0f, 550.0f,
-    5.0f, 300.0f,
-    5.0f, 550.0f
-};
-
-float Gt9[] =
-{
-    545.0f, 650.0f,
-    545.0f, 500.0f,
-    420.0f, 500.0f
-};
-
-float Gt10[] =
-{
-    545.0f, 250.0f,
-    545.0f, 500.0f,
-    420.0f, 500.0f
-};
-
-float Gt11[] =
-{
-    150.0f, 100.0f,
-    60.0f, 135.0f,
-    60.0f, 275.0f
-};
-
-float Gt12[] =
-{
-    490.0f, 275.0f,
-    490.0f, 135.0f,
-    400.0f, 100.0f
-};
-
-int addScore(int amount)
-{
-    return amount;
+    points += amount;
+    credit += amount;
 }
 
 bool TriangleCol(float t[5], float ballx, float bally)
