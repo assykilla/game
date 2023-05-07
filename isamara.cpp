79d6df4941ae82477806d867ed332f1acb31e7c4
Issa Samara <isamara@odin.cs.csubak.edu>
2023-05-07 14:28:23 -0700

    adding inital leaderboard

--- isamara.cpp
+++ isamara.cpp
@@ -9,11 +9,13 @@ using namespace std;
 #include <ctime>
 #include <cstring>
 #include <cmath>
+#include <fstream>
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include <GL/glx.h>
 #include "fonts.h"
 #include "isamara.h"
+#include "xreyes.h"
 
 //void draw_box(float, float, float, float, int, int, unsigned char);
 void prompt::set_color ( unsigned char col[3])
@@ -55,6 +57,22 @@ void draw_box(float xcoord, float ycoord, float width, float height, int xres, i
     glPopMatrix();
 }
 
+void draw_texture(GLuint image, int x, int y)
+{
+    glBindTexture(GL_TEXTURE_2D, image);
+    int w = x;
+    int h = y;
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 1.0f); glVertex2i(0, 0);
+    glTexCoord2f(0.0f, 0.0f); glVertex2i(0,  h);
+    glTexCoord2f(1.0f, 0.0f); glVertex2i( w,  h);
+    glTexCoord2f(1.0f, 1.0f); glVertex2i( w, 0);
+    glEnd();
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+}
+
 void create_text(Rect t, string text, char* temp, float xcoord, float ycoord)
 {
     t.bot = ycoord;
@@ -63,14 +81,17 @@ void create_text(Rect t, string text, char* temp, float xcoord, float ycoord)
     strcpy(temp, text.c_str());
     ggprint8b(&t, 16, 0x00fffff0, temp);
 }
-void render_menu(prompt p, Rect t, int i, int x, int y)
+void render_menu(Rect t, int i, int x, int y)
 {
+    prompt p(x/10, y/40, x/8, 3*(y/4));
     string text[4] = {"Start", "Controls", "Leaderboard", "Exit"};
     char temp[24];
     float xcoord = p.pos[0];
-    float ycoord = p.pos[1] - 60*i;
+    float ycoord = p.pos[1] - (i*100);
     //Draw boxes.
-    draw_box(p.pos[0], ycoord, p.w, p.h, x, y, p.color);
+    unsigned char c[3] = {200,200,200};
+    p.set_color(c);
+    draw_box(xcoord, ycoord, p.w, p.h, x, y, p.color);
     create_text(t, text[i], temp, xcoord, ycoord);
 }
 
@@ -101,28 +122,28 @@ void render_title(Rect t, int x, int y)
     ggprint8b(&t, 16, 0x00fffff0, text);
 }
 
-unsigned int select_option(int x, int y)
+unsigned int select_option(int x, int y, int xbutton, int ybutton)
 {
-    prompt size;
-    if (y <= size.pos[1]+size.h &&
-            x >= size.pos[0]-size.w &&
-            x <= size.pos[0]+size.w &&
-            y >= size.pos[1]-size.h) {
+    prompt size(x/10, y/40, x/8, 3*(y/4));
+    if (ybutton <= size.pos[1]+size.h &&
+            xbutton >= size.pos[0]-size.w &&
+            xbutton <= size.pos[0]+size.w &&
+            ybutton >= size.pos[1]-size.h) {
         return 1;
-    } else if (y <= size.pos[1]+size.h - 60 &&
-            x >= size.pos[0]-size.w &&
-            x <= size.pos[0]+size.w &&
-            y >= size.pos[1]-size.h - 60) {
+    } else if (ybutton <= size.pos[1]+size.h - 100 &&
+            xbutton >= size.pos[0]-size.w &&
+            xbutton <= size.pos[0]+size.w &&
+            ybutton >= size.pos[1]-size.h - 100) {
         return 2;
-    } else if (y <= size.pos[1]+size.h - 120 &&
-            x >= size.pos[0]-size.w &&
-            x <= size.pos[0]+size.w &&
-            y >= size.pos[1]-size.h - 120) {
+    } else if (ybutton <= size.pos[1]+size.h - 200 &&
+            xbutton >= size.pos[0]-size.w &&
+            xbutton <= size.pos[0]+size.w &&
+            ybutton >= size.pos[1]-size.h - 200) {
         return 3;
-    } else if (y <= size.pos[1]+size.h - 180 &&
-            x >= size.pos[0]-size.w &&
-            x <= size.pos[0]+size.w &&
-            y >= size.pos[1]-size.h - 180) {
+    } else if (ybutton <= size.pos[1]+size.h - 300 &&
+            xbutton >= size.pos[0]-size.w &&
+            xbutton <= size.pos[0]+size.w &&
+            ybutton >= size.pos[1]-size.h - 300) {
         return 4;
     } else {return 0;}
 }
@@ -180,4 +201,93 @@ void push_button(float *velocity, int x, int y)
 void update_lives(unsigned int lives) 
 {
 
+}
+
+void power_up()
+{
+
+}
+
+void check_score(int score, int score_counter, int *lives, int *multiplier)
+{
+    if (score%10000 == 0 && score != 0)
+        lives++;
+        //&lives++;
+
+}
+
+bool reset_death(int *score_counter, int *multiplier, bool death_true)
+{
+    if (!death_true)
+        return false;
+
+    //&score_counter = 0;
+    //&multiplier = 0;
+    return false;
+}
+
+//void read_leaderboard(string file, vector<string> name, vector<string> score)
+void read_leaderboard(string file, string* name, string* score)
+{
+    string line;
+    int counter = 0;
+    ifstream fin(file);
+    if (fin.fail()) {
+        cout << "Bad file input, please make sure the file path is correct"
+            << endl;
+    }
+    fin >> line;
+    while (!fin.eof()) {
+        char placeholder[4];
+        char scoreholder[100];
+        strncpy(placeholder, line.c_str(), 3);
+        name[counter] = placeholder;
+        line.erase(0,3);
+        strcpy(scoreholder, line.c_str());
+        score[counter] = scoreholder;
+        fin >> line;
+        counter++;
+    }
+
+    fin.close();
+}
+
+//void print_leaderboard(vector<string> name, vector<string> score, int x, int y)
+void print_leaderboard_boxes(int x, int y)
+{
+    prompt n(x/30, y/4, x/8, (y/2));
+    prompt s(x/10, y/4, x/2, (y/2));
+    Rect t;
+    string name = "name";
+    string score = "score";
+    char temp[24];
+    float xcoord1 = n.pos[0];
+    float xcoord2 = s.pos[0];
+    float ycoord = 7*(y/10);
+    unsigned char c[3] = {200,200,200};
+    n.set_color(c);
+    s.set_color(c);
+    //draw_box(xcoord, ycoord, p.w, p.h, x, y, p.color);
+    draw_box(xcoord1, n.pos[1], n.w, n.h, x, y, n.color);
+    draw_box(xcoord2, s.pos[1], s.w, s.h, x, y, s.color);
+    create_text(t, name, temp, xcoord1, ycoord);
+    create_text(t, score, temp, xcoord2, ycoord);
+}
+
+void print_leaderboard(string* name, string* score, int x, int y)
+{
+    Rect t;
+    prompt n(x/30, y/4, x/8, 2*(y/3));
+    prompt s(x/10, y/4, x/2, 2*(y/3));
+    char temp[100];
+    float xcoord1 = n.pos[0];
+    float xcoord2 = s.pos[0];
+    //create_text(t, text[i], temp, xcoord, ycoord);
+    for (int i=0; i<10; i++) {
+        string leader = name[i];
+        string highscore = score[i];
+        float ycoord = n.pos[1] - (i*30);
+        create_text(t, leader, temp, xcoord1, ycoord);
+        create_text(t, highscore, temp, xcoord2, ycoord);
+    }
 }
--- isamara.h
+++ isamara.h
@@ -9,20 +9,26 @@ class prompt {
         float w, h;
         float pos[2];
         unsigned char color[3];
-        void set_color ( unsigned char col[3]);
+        void set_color (unsigned char col[3]);
         prompt();
-        prompt(float, float, float, float);
+        prompt(float,float,float,float);
 };
 
 
-extern void draw_box(float, float, float, float, int, int, unsigned char);
-extern void create_text(Rect, char*, float, float);
-extern void render_menu(prompt,Rect,int,int,int);
+extern void draw_box(float,float,float,float,int,int,unsigned char);
+extern void draw_texture(GLuint,int,int);
+extern void create_text(Rect,char*,float,float);
+extern void render_menu(Rect,int,int,int);
 extern void render_map_select(int, int);
 extern void render_title(Rect t,int,int);
-extern unsigned int select_option(int,int);
-extern unsigned int select_map(int, int, int, int);
-extern void draw_button(int, int);
-extern void push_button(float*, int, int);
+extern unsigned int select_option(int,int,int,int);
+extern unsigned int select_map(int,int,int,int);
+extern void draw_button(int,int);
+extern void push_button(float*,int,int);
+extern void check_score(int,int,int*,int*);
+extern bool reset_death(int*,int*,bool);
+extern void read_leaderboard(string,string*,string*);
+extern void print_leaderboard_boxes(int,int);
+extern void print_leaderboard(string*,string*,int,int);
 
 #endif
